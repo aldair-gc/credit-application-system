b32adb5e89122ba67290489c226cd6fb82851a95
Aldair Garros Carvalho <98914031+aldair-gc@users.noreply.github.com>
2024-01-29 09:16:17 -0300

    (feat: service) create credit and customer service

--- src/main/kotlin/com/aldairgc/credit/application/system/service/impl/CustomerService.kt
+++ src/main/kotlin/com/aldairgc/credit/application/system/service/impl/CustomerService.kt
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Service
 @Service
 class CustomerService(
     private val customerRepository: CustomerRepository
-): ICustomerService {
+) : ICustomerService {
     override fun save(customer: Customer): Customer = this.customerRepository.save(customer)
 
-    override fun findById(id: Long): Customer = this.customerRepository.findById(id).orElseThrow{
+    override fun findById(id: Long): Customer = this.customerRepository.findById(id).orElseThrow {
         throw RuntimeException("Id $id not found")
     }
+
     override fun delete(id: Long) = this.customerRepository.deleteById(id)
 }
